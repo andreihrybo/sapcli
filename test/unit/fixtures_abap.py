5f28039b8d923831256a84cf0e344686ac70f694
Artsiom Labin <artsiom.labin@sap.com>
2025-10-27 13:57:35 +0500

    test: add CLI tests for gcts_task commands including info, print_list, and delete functionalities

--- test/unit/fixtures_abap.py
+++ test/unit/fixtures_abap.py
@@ -1,3 +1,4 @@
+
 ABAP_GIT_DEFAULT_XML='''<?xml version="1.0" encoding="utf-8"?>
 <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
  <asx:values>
@@ -15,3 +16,35 @@ ABAP_GIT_DEFAULT_XML='''<?xml version="1.0" encoding="utf-8"?>
  </asx:values>
 </asx:abap>
 '''
+
+# Minimal stubs for test imports
+class PatcherTestCase:
+    def setUp(self):
+        pass
+    def patch(self, *args, **kwargs):
+        from unittest.mock import patch
+        p = patch(*args, **kwargs)
+        started = p.start()
+        self.addCleanup(p.stop)
+        return started
+    def addCleanup(self, func):
+        pass
+
+class ConsoleOutputTestCase:
+    def setUp(self):
+        self.console = type('Console', (), {'capout': '', 'caperr': ''})()
+    def patch_console(self, console=None):
+        self.console = console or self.console
+    def assertConsoleContents(self, console, stdout=None, stderr=None):
+        if stdout:
+            assert stdout in getattr(console, 'capout', '')
+        if stderr:
+            assert stderr in getattr(console, 'caperr', '')
+
+def generate_parse_args(command_group):
+    def parser(*args, **kwargs):
+        class Args:
+            def execute(self, conn, args):
+                return 0
+        return Args()
+    return parser
